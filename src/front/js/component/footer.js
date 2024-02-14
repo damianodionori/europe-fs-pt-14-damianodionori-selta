bfff83298899b06f148b78eab8ba5d99099d5868
Selta7 <132694122+Selta7@users.noreply.github.com>
2024-02-14 20:35:18 +0000

    Style and footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,74 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
+import "../../styles/footer.css";
+
+
 
 export const Footer = () => (
-	<footer className=" text-center text-lg-start">
-  
-  <div className="container p-4">
-    
-    <div className="row">
-      
-      <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
-        <h4 className="text-uppercase">Footer Content</h4>
-
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
-          molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
-          voluptatem veniam, est atque cumque eum delectus sint!
-        </p>
-      </div>
-      
-
-      
-      <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 className="text-uppercase">Links</h5>
-
-        <ul className="list-unstyled mb-0">
-          <li>
-            <a href="#!" className="">Link 1</a>
-          </li>
-          <li>
-            <a href="#!" className="">Link 2</a>
-          </li>
-          <li>
-            <a href="#!" className="">Link 3</a>
-          </li>
-          <li>
-            <a href="#!" className="">Link 4</a>
-          </li>
-        </ul>
-      </div>
-     
-
-      
-      <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <h5 className="text-uppercase mb-0">Links</h5>
-
-        <ul className="list-unstyled">
-          <li>
-            <a href="#!" className="">Link 1</a>
-          </li>
-          <li>
-            <a href="#!" className="">Link 2</a>
-          </li>
-          <li>
-            <a href="#!" className="">Link 3</a>
-          </li>
-          <li>
-            <a href="#!" className="">Link 4</a>
-          </li>
-        </ul>
-      </div>
-      
+	<footer className="footer">
+    <div className="waves">
+      <div className="wave" id="wave1"></div>
+      <div className="wave" id="wave2"></div>
+      <div className="wave" id="wave3"></div>
+      <div className="wave" id="wave4"></div>
     </div>
-    
-  </div>
-  
+    <ul className="social-icon">
+      <li className="social-icon__item"><a className="social-icon__link" href="#">
+          <ion-icon name="logo-facebook"></ion-icon>
+        </a></li>
+      <li className="social-icon__item"><a className="social-icon__link" href="#">
+          <ion-icon name="logo-twitter"></ion-icon>
+        </a></li>
+      <li className="social-icon__item"><a className="social-icon__link" href="#">
+          <ion-icon name="logo-linkedin"></ion-icon>
+        </a></li>
+      <li className="social-icon__item"><a className="social-icon__link" href="#">
+          <ion-icon name="logo-instagram"></ion-icon>
+        </a></li>
+    </ul>
+    <ul className="menu">
+      <li className="menu__item"><a className="menu__link" href="#">Home</a></li>
+      <li className="menu__item"><a className="menu__link" href="#">About</a></li>
+      <li className="menu__item"><a className="menu__link" href="#">Services</a></li>
+      <li className="menu__item"><a className="menu__link" href="#">Team</a></li>
+      <li className="menu__item"><a className="menu__link" href="#">Contact</a></li>
 
-  
-  <div className="text-center p-3">
-    © 2024 Copyright:
-    <a>DioDio LTD</a>
-  </div>
-
-</footer>
+    </ul>
+    <p>&copy;2024 Dio&Dio Ltd | All Rights Reserved</p>
+  </footer>
 );
